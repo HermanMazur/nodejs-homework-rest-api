b12bf2d78664675b9d84cac74a8a0d94c039f243
HermanMazur <zlabademazur@gmail.com>
2023-01-11 06:33:12 +0200

    none done

---
+++ models/contact.js
@@ -0,0 +1,38 @@
+const { Schema, model} = require("mongoose");
+const Joi = require("joi");
+
+const contactSchema = new Schema({
+    name: {
+    type: String,
+    required: [true, "Set name for contact"],
+    },
+    email: {
+    type: String,
+    required: [true, "Set email for contact"],
+    },
+    phone: {
+    type: String,
+    required: [true, "Set phone for contact"],
+    },
+    favorite: {
+    type: Boolean,
+    default: false,
+    },
+},
+{ versionKey: false, timestamps: true }
+);
+
+const Contact = model("contact", contactSchema);
+
+const contactsSchema = Joi.object({
+name: Joi.string().required(),
+email: Joi.string().required(),
+phone: Joi.string().required(),
+favorite: Joi.boolean(),
+});
+
+
+module.exports = {
+    contactsSchema,
+    Contact
+};
