027ca8fc283cc0ce83358d538b2a393bd70d609a
HermanMazur <zlabademazur@gmail.com>
2023-01-28 16:27:33 +0200

    fix

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -22,7 +22,7 @@ const signupUser = async (req, res) => {
     const verifyEmail = {
         to: email,
         subject: "Verify you email",
-        html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Click verify email</a>`
+        html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click verify email</a>`
     }
 
     await sendEmail(verifyEmail);
